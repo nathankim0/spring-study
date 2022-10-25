301bff38359b2fbb513454d34e23bb9ffaf74ad3
nathankim0 <jinyeob07@gmail.com>
2022-10-25 17:40:08 +0900

    feat: member service 구현

---
+++ src/main/java/com/nathan/nathanspring/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.nathan.nathanspring.service;
+
+import com.nathan.nathanspring.domain.Member;
+import com.nathan.nathanspring.repository.MemberRepository;
+import com.nathan.nathanspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//비즈니스 의존적, 기획자도 이해할 수 있게 용어 선택.
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicatedMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
