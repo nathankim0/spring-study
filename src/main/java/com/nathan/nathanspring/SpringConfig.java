b8fdf3ddb25f1540d1b4f51f728f364ae0891fee
nathankim0 <jinyeob07@gmail.com>
2022-10-28 16:31:18 +0900

    feat: JdbcTemplate

--- src/main/java/com/nathan/nathanspring/SpringConfig.java
+++ src/main/java/com/nathan/nathanspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.nathan.nathanspring;
 
+import com.nathan.nathanspring.repository.JdbcTemplateMemberRepository;
 import com.nathan.nathanspring.repository.MemberRepository;
 import com.nathan.nathanspring.repository.MemoryMemberRepository;
 import com.nathan.nathanspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
